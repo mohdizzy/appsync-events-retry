1423fbce25e03fc79b5e0898a71ef21b79c50548
Mohammed Izzy <mohammed@serverlessguru.com>
2024-11-20 15:37:32 +0530

    wip

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,4 +1,4 @@
-const { Logger } = require("logger");
+const { Logger } = require("@serverless-guru/logger");
 const logger = new Logger("AppSync_Events", "myEventApp");
 const metricUnits = Logger.METRIC_UNITS;
 module.exports = { logger, metricUnits };
